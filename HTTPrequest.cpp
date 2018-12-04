b9b6c1e360fc5dcee9a89ae999554f600280e475
ghjuliasialelli <45049580+ghjuliasialelli@users.noreply.github.com>
2018-12-04 18:13:21 +0100

    To fetch data from an URL
    to compile, i do : $ g++ -std=c++14 -lcurlpp -lcurl HTTPrequest.cpp -o main

--- HTTPrequest.cpp
+++ HTTPrequest.cpp
@@ -1,33 +1,49 @@
-/**
-* \file
-* The most simple example.
-* 
-*/
-
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 
+#include <iostream>
+#include <fstream> // Stream class to both read and write from/to files.
 
 using namespace curlpp::options;
+using namespace std ; 
 
 int main(int, char **)
 {
 	try
 	{
-		// That's all that is needed to do cleanup of used resources (RAII style).
+		// That's all that is needed to do cleanup of used resources.
 		curlpp::Cleanup myCleanup;
 
+		// Set the URL.
+		curlpp::options::Url myUrl(std::string("http://inspirehep.net/textmining/?ln=en&fbclid=IwAR1JJVUiVazrFK6hgM_gWiPJQw1eP_wLWWC7bnjMrg6BqQ-EObuo2Acj048"));
+		
 		// Our request to be sent.
 		curlpp::Easy myRequest;
 
-		// Set the URL.
-		myRequest.setOpt<Url>("https://en.wikipedia.org/wiki/Percent-encoding");
-		// basic example : http://example.com
 
-		// Send request and get a result.
-		// By default the result goes to standard output.
+		// OPTION 1 : the content of that URL will be printed in std::cout
+		myRequest.setOpt(myUrl);
 		myRequest.perform();
+
+
+		// OPTION 2 : put the content of the URL within a string stream
+		/* 
+		std::ostringstream os;
+		curlpp::options::WriteStream ws(&os);
+		myRequest.setOpt(ws);
+		myRequest.perform();  
+
+		std::cout << os << std::endl ;  
+		*/
+
+
+		// OPTION 3 : writing the content of the URL onto a file
+		/* 
+		std::ofstream outfile ("test.txt");
+  		outfile << os << std::endl;
+  		outfile.close();
+		*/ 
 	}
 
 	catch(curlpp::RuntimeError & e)
